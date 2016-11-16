9f043686c340810cf20f14e0e2a5517e87165ea8
Matthias Crauwels <matthias.crauwels@ugent.be>
2016-11-16 23:08:36 +0100

    remove ugent-keyring

--- examples/init.pp
+++ examples/init.pp
@@ -1,11 +1,4 @@
 # lint:ignore:80chars
-exec { 'ugent-keyring':
-  command => 'wget http://debs.ugent.be/debian/pool/main/u/ugent-keyring/ugent-keyring_0.2-2_all.deb && dpkg -i ugent-keyring_0.2-2_all.deb && apt-get update',
-  cwd     => '/root',
-  creates => '/root/ugent-keyring_0.2-2_all.deb',
-  path    => ['/usr/local/sbin','/usr/local/bin','/usr/sbin','/usr/bin','/sbin','/bin','/opt/puppetlabs/bin'],
-  unless  => 'test -f /root/ugent-keyring_0.2-2_all.deb',
-}->
 apt::source{ 'debs_jessie_mysql_dev':
   comment  => 'MySQL-dev repo',
   location => 'http://debs.ugent.be/debian',
