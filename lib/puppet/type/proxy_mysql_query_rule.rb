04c9b4eb2527b22af06284ea0f76bf64d28661f2
Matthias Crauwels <matthias.crauwels@ugent.be>
2017-02-06 21:25:31 +0100

    added types for scheduler and mysql_query_rules (providers to come)

---
+++ lib/puppet/type/proxy_mysql_query_rule.rb
@@ -0,0 +1,180 @@
+Puppet::Type.newtype(:proxy_mysql_query_rule) do
+  @doc = 'Manage a ProxySQL mysql_query_rules entry.'
+
+  ensurable
+
+  autorequire(:file) { '/root/.my.cnf' }
+  autorequire(:class) { 'mysql::client' }
+
+  validate do
+    fail('rule_id parameter is required.') if self[:rule_id].nil?
+    fail('name must match \'mysql_query_rule-\'<rule_id> format') if self[:name] != "mysql_query_rule-#{self[:rule_id]}"
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'query rule name'
+  end
+
+  newparam(:load_to_runtime) do
+    desc 'Load this entry to the active runtime.'
+    defaultto :true
+    newvalues(:true, :false)
+  end
+
+  newparam(:save_to_disk) do
+    desc 'Perist this entry to the disk.'
+    defaultto :true
+    newvalues(:true, :false)
+  end
+
+  newproperty(:rule_id) do
+    desc 'The id of the query rule.'
+    newvalue(/\d+/)
+  end
+
+  newproperty(:active) do
+    desc "Is the rule active or not."
+    defaultto 0
+    newvalue(/[01]/)
+  end
+
+  newproperty(:username) do
+    desc "Username to apply this rule to."
+    newvalue(/\w+/)
+  end
+
+  newproperty(:schemaname) do
+    desc "Schema to apply this rule to."
+    newvalue(/\w+/)
+  end
+
+  newproperty(:flagIN) do
+    desc 'Used to chain rules. This is the id of the previous rule to apply'
+    defaultto 0
+    newvalue(/\d+/)
+  end
+
+  newproperty(:flagOUT) do
+    desc 'Used to chain rules. This is the id of the next rule to apply'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:apply) do
+    desc 'Used to chain rules.'
+    defaultto 0
+    newvalue(/[01]/)
+  end
+
+  newproperty(:client_addr) do
+    desc 'Match traffic from a certain address.'
+    defaultto nil
+  end
+
+  newproperty(:proxy_addr) do
+    desc 'Match incoming traffic on a specific local address.'
+    defaultto nil
+  end
+
+  newproperty(:proxy_port) do
+    desc 'Match incoming traffic on a specific local port.'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:digest) do
+    desc 'match queries with a specific digest, as returned by stats_mysql_query_digest.digest'
+    defaultto nil
+    newvalue(/\w+/)
+  end
+
+  newproperty(:match_digest) do
+    desc 'regular expression that matches the query digest'
+    defaultto nil
+    newvalue(/\w+/)
+  end
+
+  newproperty(:match_pattern) do
+    desc 'regular expression that matches the query text'
+    defaultto nil
+    newvalue(/\w+/)
+  end
+
+  newproperty(:replace_pattern) do
+    desc 'this is the pattern with which to replace the matched pattern.'
+    defaultto nil
+    newvalue(/\w+/)
+  end
+
+  newproperty(:negate_match_pattern) do
+    desc 'if this is set to 1, only queries not matching the query text will be considered as a match. This acts as a NOT operator in front of the regular expression matching against match_pattern or match_digest.'
+    defaultto 0
+    newvalue(/[01]/)
+  end
+
+  newproperty(:destination_hostgroup) do
+    desc "The hostgroup to send this query to."
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:cache_ttl) do
+    desc 'The amount of miliseconds to cache the result of this query.'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:reconnect) do
+    desc 'feature currently not in use.'
+    defaultto nil
+    newvalue(/[01]/)
+  end
+
+  newproperty(:timeout) do
+    desc 'The maximum amount of miliseconds in which the matched or rewritten query should be executed.'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:retries) do
+    desc 'the maximum number of times a query needs to be re-executed in case of detected failure during the execution of the query.'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:delay) do
+    desc 'number of milliseconds to delay the execution of the query.'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:error_msg) do
+    desc 'query will be blocked, and the specified error_msg will be returned to the clientß.'
+    defaultto nil
+    newvalue(/\w+/)
+  end
+
+  newproperty(:log) do
+    desc 'query will be logged.'
+    defaultto nil
+    newvalue(/[01]/)
+  end
+
+  newproperty(:mirror_hostgroup) do
+    desc "see https://github.com/sysown/proxysql/blob/master/doc/mirroring.md."
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:mirror_flagOUT) do
+    desc 'see https://github.com/sysown/proxysql/blob/master/doc/mirroring.md'
+    defaultto nil
+    newvalue(/\d+/)
+  end
+
+  newproperty(:comment) do
+    desc "free form text field, usable for a descriptive comment of the query rule."
+    newvalue(/[\w+]/)
+  end
+
+end
---
+++ lib/puppet/type/proxy_scheduler.rb
@@ -0,0 +1,71 @@
+Puppet::Type.newtype(:proxy_scheduler) do
+  @doc = 'Manage a ProxySQL scheduler entry.'
+
+  ensurable
+
+  autorequire(:file) { '/root/.my.cnf' }
+  autorequire(:class) { 'mysql::client' }
+
+  validate do
+    fail('id parameter is required.') if self[:id].nil?
+    fail('filename parameter is required.') if self[:ensure] == :present and self[:filename].nil?
+    fail('name must match \'scheduler-\'<id> format') if self[:name] != "scheduler-#{self[:rule_id]}"
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'scheduler name'
+  end
+
+  newproperty(:id) do
+    desc 'The id of the scheduler entry.'
+    newvalue(/\d+/)
+  end
+
+  newproperty(:active) do
+    desc "Is the scheduler active or not."
+    defaultto 0
+    newvalue(/[01]/)
+  end
+
+  newproperty(:interval_ms) do
+    desc 'How often (in millisecond) the job will be started.'
+    defaultto 10000
+    newvalue(/\d+/)
+  end
+
+  newproperty(:filename) do
+    desc 'Filename of the script to run. (required)'
+    newvalue(/\w+/)
+  end
+
+  newproperty(:arg1) do
+    desc "optional argument to pass to the script."
+    newvalue(/[\w+]/)
+  end
+
+  newproperty(:arg2) do
+    desc "optional argument to pass to the script."
+    newvalue(/[\w+]/)
+  end
+
+  newproperty(:arg3) do
+    desc "optional argument to pass to the script."
+    newvalue(/[\w+]/)
+  end
+
+  newproperty(:arg4) do
+    desc "optional argument to pass to the script."
+    newvalue(/[\w+]/)
+  end
+
+  newproperty(:arg5) do
+    desc "optional argument to pass to the script."
+    newvalue(/[\w+]/)
+  end
+
+  newproperty(:comment) do
+    desc "optional comment."
+    newvalue(/[\w+]/)
+  end
+
+end
