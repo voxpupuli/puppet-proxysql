e573bab084e5a17127b8137a2bf651514d5fb956
Matthias Crauwels <matthias.crauwels@ugent.be>
2017-02-07 14:17:09 +0100

    use the custom types in the example

--- examples/init.pp
+++ examples/init.pp
@@ -5,48 +5,57 @@ apt::source{ 'debs_jessie_mysql_dev':
   repos    => 'mysql-dev',
 }->
 class { '::proxysql':
-  listen_port              => 3306,
-  admin_password           => 'SuperSecretPassword',
-  override_config_settings => {
-    mysql_servers                => {
-      'mysql1' => {
-        address   => '192.168.33.31',
-        port      => 3306,
-        hostgroup => 31,
-      },
-      'mysql2' => {
-        address   => '192.168.33.32',
-        port      => 3306,
-        hostgroup => 31,
-      },
-      'mysql3' => {
-        address   => '192.168.33.33',
-        port      => 3306,
-        hostgroup => 31,
-      },
-      'mysql4' => {
-        address   => '192.168.33.34',
-        port      => 3306,
-        hostgroup => 31,
-      },
-      'mysql5' => {
-        address   => '192.168.33.35',
-        port      => 3306,
-        hostgroup => 31,
-      },
-    },
-    mysql_replication_hostgroups => {
-      'hostgroup1' => {
-        writer_hostgroup => 30,
-        reader_hostgroup => 31,
-        comment          => 'Replication Group 1',
-      },
-      'hostgroup2' => {
-        writer_hostgroup => 20,
-        reader_hostgroup => 21,
-        comment          => 'Replication Group 2',
-      },
-    }
-  }
+  listen_port    => 3306,
+  admin_password => 'SuperSecretPassword',
+}
+
+proxy_mysql_server { '192.168.33.31:3306-31':
+  hostname     => '192.168.33.31',
+  port         => 3306,
+  hostgroup_id => 31,
+}
+proxy_mysql_server { '192.168.33.32:3306-31':
+  hostname     => '192.168.33.32',
+  port         => 3306,
+  hostgroup_id => 31,
+}
+proxy_mysql_server { '192.168.33.33:3306-31':
+  hostname     => '192.168.33.33',
+  port         => 3306,
+  hostgroup_id => 31,
+}
+proxy_mysql_server { '192.168.33.34:3306-31':
+  hostname     => '192.168.33.34',
+  port         => 3306,
+  hostgroup_id => 31,
+}
+proxy_mysql_server { '192.168.33.35:3306-31':
+  hostname     => '192.168.33.35',
+  port         => 3306,
+  hostgroup_id => 31,
+}
+
+proxy_mysql_replication_hostgroup { '30-31':
+  writer_hostgroup => 30,
+  reader_hostgroup => 31,
+  comment          => 'Replication Group 1',
+}
+proxy_mysql_replication_hostgroup { '20-21':
+  writer_hostgroup => 20,
+  reader_hostgroup => 21,
+  comment          => 'Replication Group 2',
+}
+
+proxy_mysql_user { 'tester':
+  password          => 'testerpwd',
+  default_hostgroup => 30,
+}
+
+proxy_mysql_query_rule { 'mysql_query_rule-1':
+  rule_id               => 1,
+  match_pattern         => '^SELECT',
+  apply                 => 1,
+  active                => 1,
+  destination_hostgroup => 31,
 }
 # lint:endignore
