61c699c030bad97ec8b63bc32ee4f125c672b188
Maksim Fedotov <m_fedotov@wargaming.net>
2018-08-23 17:37:08 +0300

    update tests

--- spec/classes/proxysql_spec.rb
+++ spec/classes/proxysql_spec.rb
@@ -13,14 +13,19 @@ describe 'proxysql' do
           it { is_expected.to compile.with_all_deps }
 
           it { is_expected.to contain_class('proxysql::params') }
-          it { is_expected.to contain_class('proxysql::repo').that_comes_before('Class[proxysql::install]') }
-          it { is_expected.to contain_class('proxysql::install').that_comes_before('Class[proxysql::config]') }
-          it { is_expected.to contain_class('proxysql::config').that_comes_before('Class[proxysql::service]') }
-          it { is_expected.to contain_class('proxysql::service').that_subscribes_to('Class[proxysql::install]') }
 
           it { is_expected.to contain_anchor('::proxysql::begin').that_comes_before('Class[proxysql::repo]') }
+          it { is_expected.to contain_class('proxysql::repo').that_comes_before('Class[proxysql::config]') }
+          it { is_expected.to contain_class('proxysql::config').that_comes_before('Class[proxysql::install]') }
+          it { is_expected.to contain_class('proxysql::install').that_comes_before('Class[proxysql::service]') }
+          it { is_expected.to contain_class('proxysql::service').that_comes_before('Class[proxysql::admin_credentials]') }
+          it { is_expected.to contain_class('proxysql::admin_credentials').that_comes_before('Class[proxysql::reload_config]') }
+          it { is_expected.to contain_class('proxysql::reload_config').that_comes_before('Class[proxysql::cluster]') }
+          it { is_expected.to contain_class('proxysql::cluster').that_comes_before('Class[proxysql::configure]') }
+          it { is_expected.to contain_class('proxysql::configure').that_comes_before('Anchor[::proxysql::end]') }
           it { is_expected.to contain_anchor('::proxysql::end') }
-          it { is_expected.to contain_class('proxysql::service').that_comes_before('Anchor[::proxysql::end]') }
+
+          it { is_expected.to contain_class('proxysql::service').that_subscribes_to('Class[proxysql::install]') }
 
           it { is_expected.to contain_class('proxysql::install').that_notifies('Class[proxysql::service]') }
 
@@ -47,11 +52,11 @@ describe 'proxysql' do
           end
 
           it do
-            is_expected.to contain_file('proxysql-config-file').with(ensure: 'file',
-                                                                     owner: sys_user,
-                                                                     group: sys_group,
-                                                                     mode: '0640',
-                                                                     path: '/etc/proxysql.cnf')
+            is_expected.to contain_file('proxysql-main-config-file').with(ensure: 'file',
+                                                                          owner: sys_user,
+                                                                          group: sys_group,
+                                                                          mode: '0640',
+                                                                          path: '/etc/proxysql.cnf')
           end
 
           it do
@@ -64,9 +69,9 @@ describe 'proxysql' do
 
           it do
             is_expected.to contain_file('root-mycnf-file').with(ensure: 'file',
-                                                                owner: sys_user,
-                                                                group: sys_group,
-                                                                mode: '0400',
+                                                                owner: 'root',
+                                                                group: 'root',
+                                                                mode: '0640',
                                                                 path: '/root/.my.cnf')
           end
 
