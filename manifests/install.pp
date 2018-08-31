020aac45c37d0d3ce6a78a22679b2eca57606962
Maksim Fedotov <m_fedotov@wargaming.net>
2018-08-31 18:27:31 +0300

    change :: to : after $

--- manifests/install.pp
+++ manifests/install.pp
@@ -44,7 +44,7 @@ class proxysql::install {
   }
 
   class { '::mysql::client':
-    package_name    => $::proxysql::mysql_client_package_name,
+    package_name    => $proxysql::mysql_client_package_name,
     bindings_enable => false,
   }
 
