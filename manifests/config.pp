fe84efddee0f082f9ba758b233ae6dbb65632e6e
Maksim Fedotov <m.a.fedotov@gmail.com>
2018-09-01 17:04:08 +0300

    apply review notes. Change $:: to $. Use facts[]. Change erb template to epp

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,6 +6,8 @@ class proxysql::config {
 
   $config_settings = $proxysql::config_settings
   $proxy_config_file = $proxysql::proxy_config_file
+  $split_config = $proxysql::split_config
+
   if $proxysql::manage_config_file  {
     file { 'proxysql-config-file':
       ensure                  => file,
@@ -22,7 +24,7 @@ class proxysql::config {
     file { 'proxysql-proxy-config-file':
       ensure                  => file,
       path                    => $proxysql::proxy_config_file,
-      content                 => template('proxysql/proxysql_proxy.cnf.erb'),
+      content                 => epp('proxysql/proxysql_proxy.cnf.epp', { config_settings => $config_settings }),
       mode                    => '0640',
       owner                   => $proxysql::sys_owner,
       group                   => $proxysql::sys_group,
