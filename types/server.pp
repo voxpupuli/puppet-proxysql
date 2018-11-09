e7bcbf6fbdbc74a009543287a3642585a0aad5a9
MaxFedotov <m.a.fedotov@gmail.com>
2018-11-09 14:07:32 +0300

    fix bug in types that int[0,1] were boolean

--- types/server.pp
+++ types/server.pp
@@ -6,7 +6,7 @@ type Proxysql::Server = Array[Hash[String, Struct[{ Optional[port]              
                                                     Optional[compression]         => Integer,
                                                     Optional[max_connections]     => Integer,
                                                     Optional[max_replication_lag] => Integer,
-                                                    Optional[use_ssl]             => Boolean,
+                                                    Optional[use_ssl]             => Integer[0,1],
                                                     Optional[max_latency_ms]      => Integer,
                                                     Optional[comment]             => String[1], }],1,1]]
 # lint:endignore                                                 
--- types/user.pp
+++ types/user.pp
@@ -1,13 +1,13 @@
 # lint:ignore:2sp_soft_tabs
 type Proxysql::User = Array[Hash[String, Struct[{ password                         => String[1],
                                                   default_hostgroup                => Integer,
-                                                  Optional[active]                 => Integer,
-                                                  Optional[use_ssl]                => Integer,
+                                                  Optional[active]                 => Integer[0,1],
+                                                  Optional[use_ssl]                => Integer[0,1],
                                                   Optional[default_schema]         => String[1],
-                                                  Optional[schema_locked]          => Integer,
-                                                  Optional[transaction_persistent] => Integer,
-                                                  Optional[fast_forward]           => Integer,
-                                                  Optional[backend]                => Integer,
-                                                  Optional[frontend]               => Integer,
+                                                  Optional[schema_locked]          => Integer[0,1],
+                                                  Optional[transaction_persistent] => Integer[0,1],
+                                                  Optional[fast_forward]           => Integer[0,1],
+                                                  Optional[backend]                => Integer[0,1],
+                                                  Optional[frontend]               => Integer[0,1],
                                                   Optional[max_connections]        => Integer, }],1,1]]
-# lint:endignore                                                   
+# lint:endignore
