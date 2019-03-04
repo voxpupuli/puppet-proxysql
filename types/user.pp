8d519a5b9d2988844d0df2100b6ed1090617c21b
Alexander Over <godfatherofhell@gmail.com>
2019-03-05 00:04:21 +0100

    Fix `Proxysql::User` data type

--- types/user.pp
+++ types/user.pp
@@ -1,13 +1,13 @@
 # lint:ignore:2sp_soft_tabs
 type Proxysql::User = Array[Hash[String, Struct[{ password                         => String[1],
                                                   default_hostgroup                => Integer,
-                                                  Optional[active]                 => Boolean,
-                                                  Optional[use_ssl]                => Boolean,
+                                                  Optional[active]                 => Integer,
+                                                  Optional[use_ssl]                => Integer,
                                                   Optional[default_schema]         => String[1],
-                                                  Optional[schema_locked]          => Boolean,
-                                                  Optional[transaction_persistent] => Boolean,
-                                                  Optional[fast_forward]           => Boolean,
-                                                  Optional[backend]                => Boolean,
-                                                  Optional[frontend]               => Boolean,
+                                                  Optional[schema_locked]          => Integer,
+                                                  Optional[transaction_persistent] => Integer,
+                                                  Optional[fast_forward]           => Integer,
+                                                  Optional[backend]                => Integer,
+                                                  Optional[frontend]               => Integer,
                                                   Optional[max_connections]        => Integer, }],1,1]]
 # lint:endignore                                                   
