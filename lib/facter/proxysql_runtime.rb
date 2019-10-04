688621f290dab228ef0c45e1db36a361916eb76d
Alexander Fisher <alex@linfratech.co.uk>
2019-10-04 15:48:49 +0100

    Add `proxysql_runtime` fact
    Returns a hash of containing ProxySQL runtime tables.
    Requires `mysql2` gem in puppet environment.

---
+++ lib/facter/proxysql_runtime.rb
@@ -0,0 +1,41 @@
+Facter.add(:proxysql_runtime) do
+  client = nil
+
+  confine :proxysql_version do |version|
+    version =~ %r{^2}
+  end
+
+  confine do
+    begin
+      require 'mysql2'
+    rescue LoadError
+      next
+    end
+
+    begin
+      client = Mysql2::Client.new(default_file: '/root/.my.cnf')
+    rescue Mysql2::Error::ConnectionError => e
+      Facter.debug(e.inspect)
+    end
+    client
+  end
+
+  setcode do
+    fact = {}
+    [
+      # tables that might include unencrypted credentials have not been included here.
+      'runtime_mysql_servers',
+      'runtime_mysql_aws_aurora_hostgroups',
+      'runtime_mysql_galera_hostgroups',
+      'runtime_mysql_group_replication_hostgroups',
+      'runtime_mysql_query_rules',
+      'runtime_mysql_query_rules_fast_routing',
+      'runtime_mysql_replication_hostgroups',
+      'runtime_proxysql_servers'
+    ].each do |table|
+      results = client.query("SELECT * FROM #{table}")
+      fact[table] = results.map { |row| row }
+    end
+    fact
+  end
+end
