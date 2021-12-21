5ed6249debc657651d5ab4d68ab3dd0e81f4bfcd
enno0815de <mail@enricomeinel.de>
2021-12-21 23:23:16 +0100

    added version absent (by @jadestorm)

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -25,19 +25,37 @@ class proxysql::repo {
           * => $repo,
         }
 
-        $purge_repo = $proxysql::version ? {
-          # 2.3
-          /^2\.3\./ => $proxysql::params::repo22['name'],
-          # 2.2
-          /^2\.2\./ => $proxysql::params::repo21['name'],
-          # 2.1
-          /^2\.1\./ => $proxysql::params::repo20['name'],
-          # 2.0
-          /^2\.0\./ => $proxysql::params::repo14['name'],
-          # 1.4
-          /^1\.4\./ => $proxysql::params::repo20['name'],
+        if($proxysql::version !~ /^2\.3\./) {
+          yumrepo { $proxysql::params::repo23['name']:
+            ensure => absent,
+          }
         }
-        yumrepo { ['proxysql_repo', $purge_repo]:
+
+        if($proxysql::version !~ /^2\.2\./) {
+          yumrepo { $proxysql::params::repo22['name']:
+            ensure => absent,
+          }
+        }
+
+        if($proxysql::version !~ /^2\.1\./) {
+          yumrepo { $proxysql::params::repo21['name']:
+            ensure => absent,
+          }
+        }
+
+        if($proxysql::version !~ /^2\.0\./) {
+          yumrepo { $proxysql::params::repo20['name']:
+            ensure => absent,
+          }
+        }
+
+        if($proxysql::version !~ /^1\.4\./) {
+          yumrepo { $proxysql::params::repo14['name']:
+            ensure => absent,
+          }
+        }
+
+        yumrepo { 'proxysql_repo':
           ensure => absent,
         }
       }
