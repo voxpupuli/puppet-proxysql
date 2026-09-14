1ac6a09fc3ac2784cd3ff6b3bb525d6b852d000c
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-13 20:07:19 -0400

    replace deprecated calls with Facter::Core::Execution
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/proxysql_version.rb
+++ lib/facter/proxysql_version.rb
@@ -3,8 +3,8 @@
 Facter.add(:proxysql_version) do
   confine kernel: 'Linux'
   setcode do
-    if Facter::Util::Resolution.which('proxysql')
-      proxysql_version = Facter::Util::Resolution.exec('proxysql --version 2>&1')
+    if Facter::Core::Execution.which('proxysql')
+      proxysql_version = Facter::Core::Execution.execute('proxysql --version 2>&1')
       %r{ProxySQL version (\d+\.\d+\.\d+.*),}.match(proxysql_version)[1]
     end
   end
--- spec/unit/facter/proxysql_version_spec.rb
+++ spec/unit/facter/proxysql_version_spec.rb
@@ -9,8 +9,9 @@ describe Facter::Util::Fact.to_s do
 
   describe 'proxysql_version' do
     before do
-      allow(Facter::Util::Resolution).to receive(:which).with('proxysql').and_return(true)
-      allow(Facter::Util::Resolution).to receive(:exec).with('proxysql --version 2>&1').and_return('ProxySQL version 2.0.4-116-g7d371cf2, codename Truls')
+      allow(Facter::Core::Execution).to receive(:which).with('proxysql').and_return(true)
+      allow(Facter::Core::Execution).to receive(:execute).with(a_string_including('uname -m &&'), any_args).and_return("x86_64\nlocalhost\nx86_64\n5.15.0\nLinux\n#1 SMP")
+      allow(Facter::Core::Execution).to receive(:execute).with('proxysql --version 2>&1').and_return('ProxySQL version 2.0.4-116-g7d371cf2, codename Truls')
     end
 
     it {
